24f5379716a6df9669bfff275e9ecf1eec6a5377
Jonathan Sands <cosmictails@gmail.com>
2017-09-16 13:26:06 +0800

    MyEventReceiver class separated from main.cpp

---
+++ ICT398Project/ICT398Project/source/MyEventReceiver.cpp
@@ -0,0 +1,38 @@
+		/** MyEventReceiver.cpp - Receives events and handles them
+        * @class MyEventReceiver class
+        * @brief contains code that handles any events that occur
+		* @TODO implement event handling for a key press and left-click
+        * @author Jonathan Sands
+        * modified 16/09/2017
+        */
+#include <irrlicht.h>
+using namespace irr;
+class MyEventReceiver : public IEventReceiver
+{
+public:
+    // This is the one method that we have to implement
+    virtual bool OnEvent(const SEvent& event)
+    {
+        // Remember whether each key is down or up
+        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+
+        return false;
+    }
+
+    // This is used to check whether a key is being held down
+    virtual bool IsKeyDown(EKEY_CODE keyCode) const
+    {
+        return KeyIsDown[keyCode];
+    }
+    
+    MyEventReceiver()
+    {
+        for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+            KeyIsDown[i] = false;
+    }
+
+private:
+    // We use this array to store the current state of each key
+    bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
